c9f1278fb157bf5320f4d951590a5dfbc055b693
Samiullah-Kalhoro <kalhorosami5@gmail.com>
2022-12-11 00:20:25 +0500

    App Bar

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,38 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-    
+    return MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.white,
+          appBar: AppBar(
+            leading: const Icon(
+              Icons.arrow_back_ios,
+              color: Colors.blue,
+            ),
+            backgroundColor: Colors.white,
+            elevation: 0,
+            centerTitle: true,
+            title: const Text(
+              "Market Place",
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.lightBlue),
+            ),
+            actions: [
+              IconButton(
+                icon: const Icon(Icons.search),
+                color: Colors.blue,
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: const Icon(Icons.person),
+                color: Colors.blue,
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
