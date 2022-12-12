a978b5b53c2ded678ea92477a06711c8b9b59a61
Samiullah-Kalhoro <kalhorosami5@gmail.com>
2022-12-12 17:33:07 +0500

    Used ListView

--- lib/components/products_card.dart
+++ lib/components/products_card.dart
@@ -50,51 +50,49 @@ class ProductsCard extends StatelessWidget {
     return Container(
       height: 400,
       margin: const EdgeInsets.symmetric(vertical: 10),
-      child: SingleChildScrollView(
-        child: Column(
-          children: products.map((pro) {
-            return Row(
-              children: [
-                Expanded(
-                  child: Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15),
-                      side: const BorderSide(
-                        style: BorderStyle.solid,
-                        width: 1.5,
-                        color: Color.fromARGB(150, 1, 51, 84),
-                      ),
-                    ),
-                    color: const Color.fromARGB(199, 1, 39, 65),
-                    child: Row(
-                      children: [
-                        Container(
-                          decoration: const BoxDecoration(
-                            color: Colors.white,
-                          ),
-                          width: 70.0,
-                          height: 70.0,
-                          child: pro.image,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 10.0),
-                          child: Text(
-                            pro.name,
-                            style: const TextStyle(
-                              color: Colors.white,
-                              fontSize: 15,
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                        ),
-                      ],
+      child: ListView(
+        children: products.map((pro) {
+          return Row(
+            children: [
+              Expanded(
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    side: const BorderSide(
+                      style: BorderStyle.solid,
+                      width: 1.5,
+                      color: Color.fromARGB(150, 1, 51, 84),
                     ),
                   ),
+                  color: const Color.fromARGB(199, 1, 39, 65),
+                  child: Row(
+                    children: [
+                      Container(
+                        decoration: const BoxDecoration(
+                          color: Colors.white,
+                        ),
+                        width: 70.0,
+                        height: 70.0,
+                        child: pro.image,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 10.0),
+                        child: Text(
+                          pro.name,
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 15,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ],
-            );
-          }).toList(),
-        ),
+              ),
+            ],
+          );
+        }).toList(),
       ),
     );
   }
